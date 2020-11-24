3f260eb5317bd21bfc1f0e88c9743d2de6cf22a4
fmy1995 <fmy.kymz444@gmail.com>
2020-11-24 02:24:13 +0000

    Initial Commit

---
+++ BOOKERS/app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+   @book= Book.find(params[:id]) 
+  end
+
+  def new
+    @blog = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path(b.id)
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
